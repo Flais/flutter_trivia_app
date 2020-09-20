46ab83ff50ef68c36aab93eb9929e72a85475a42
João Pereira <joaocarlospereira.jp@gmail.com>
2020-09-20 03:06:36 -0300

    Adicionado o teste do NumbersAPIGetNumberTriviaDataSource

--- test/app/modules/home/external/data_sources/numbersapi_get_random_number_trivia_data_source_test.dart
+++ test/app/modules/home/external/data_sources/numbersapi_get_random_number_trivia_data_source_test.dart
@@ -1,19 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:trivia_app_flutter/app/fixtures/api_success_return.dart';
-import 'package:trivia_app_flutter/app/modules/home/external/data_sources/numbersapi_get_number_trivia_data_source.dart';
-import 'package:trivia_app_flutter/app/modules/home/infra/external_interfaces/data_souces_interfaces/number_trivia_data_source_interface.dart';
+import 'package:trivia_app_flutter/app/modules/home/external/data_sources/numbersapi_get_random_number_trivia_data_source.dart';
+import 'package:trivia_app_flutter/app/modules/home/infra/external_interfaces/data_souces_interfaces/random_number_trivia_data_source_interface.dart';
 import 'package:trivia_app_flutter/app/modules/home/infra/external_interfaces/drivers_interfaces/http_client.dart';
 
 class MockHttpClient extends Mock implements IHttpClient {}
 
 void main() {
-  INumberTriviaDataSource dataSource;
+  IRandomNumberTriviaDataSource dataSource;
   IHttpClient mockHttpClient;
 
   setUp(() {
     mockHttpClient = MockHttpClient();
-    dataSource = NumbersAPIGetNumberTriviaDataSource(mockHttpClient);
+    dataSource = NumbersAPIGetRandomNumberTriviaDataSource(mockHttpClient);
   });
 
   test('Should return a instance of [Map<String, dynamic>]', () async {
@@ -21,8 +21,7 @@ void main() {
     when(mockHttpClient.get(any)).thenAnswer((_) => Future.value(tApiSuccessReturn));
 
     //Act
-    //Could be any number as parameter
-    final tResult = await dataSource.getTriviaData(numberTrivia: 1);
+    final tResult = await dataSource.getRandomTriviaData();
 
     expect(tResult, isInstanceOf<Map<String, dynamic>>());
   });
